7b3f588a8db40546b8e92b7b666581b5336da494
201411167 <rlarlejr103@gmail.com>
2019-08-13 13:34:45 +0900

    [binary search] 2842 added

--- ACMICPC/graph_algorithm/2842.cpp
+++ ACMICPC/graph_algorithm/2842.cpp
@@ -31,7 +31,7 @@ void input() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> height[i][j];
-			heights.push_back({ height[i][j]});
+			heights.push_back({ height[i][j] });
 		}
 	}
 }
